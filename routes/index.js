1d20556ff28839f41907ef8e1fd478230a8c86c6
Nick Cave <nxdmedia@googlemail.com>
2020-12-29 17:53:05 +0000

    Mobile styling for homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET all work page. */
+router.get('/work', function(req, res, next) {
+  res.render('work');
 });
 
 module.exports = router;
