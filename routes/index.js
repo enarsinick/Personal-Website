6fc683bfd0816c97dc37170033fd83a9ef181b02
Nick Cave <nxdmedia@googlemail.com>
2021-04-26 18:06:51 +0100

    Removed border radius on all images etc, and fixed bug with project tiles

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,16 @@ var express = require('express');
 var router = express.Router();
 const { featured_projects } = require('../data/data.json');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // Loop over each feature project and add to array
-  // then render that into the index view
-  const projects = [];
-  featured_projects.forEach(project => { projects.push(project)});
-  res.render('index', {projects});
-});
-
-/* GET about page. */
-router.get('/about', function(req, res, next) {
+// Selects random projects for DB
+const randProject = (num) => {
   let relatedProjects = [];
   let counter = 0;
   let index;
   let prevIndex;
 
-  // Pick 2 random projects for JSON file that aren't 
+  // Pick random projects for JSON file that aren't 
   // the same as the one being viewed
-  while (counter <= 1) {
+  while (counter <= num) {
     index = (Math.floor(Math.random() * featured_projects.length));
     if (index !== prevIndex) {
       relatedProjects.push(featured_projects[index]);
@@ -29,8 +20,17 @@ router.get('/about', function(req, res, next) {
     }
   }
 
-  // Render the page
-  res.render('about', { "related": relatedProjects });
+  return relatedProjects;
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {projects: randProject(3)});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { related: randProject(1) });
 });
 
 module.exports = router;
